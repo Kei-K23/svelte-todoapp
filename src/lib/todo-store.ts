a69f690cdb70cbd1a2a27ddf91574f69186eafb0
Kei-K23 <keiksl2301@gmail.com>
2024-10-22 21:13:02 +0630

    Setup todo store

---
+++ src/lib/todo-store.ts
@@ -0,0 +1,32 @@
+import { writable } from "svelte/store";
+
+export interface Todo {
+  id: string;
+  text: string;
+  completed: boolean;
+}
+
+const createTodoStore = () => {
+  const { update, subscribe } = writable<Todo[]>([]);
+
+  return {
+    subscribe,
+    addTodo: (text: string) =>
+      update((todos) => [
+        ...todos,
+        { id: `${todos.length}-${Date.now()}`, text, completed: false },
+      ]),
+    toggleTodo: (id: string) =>
+      update((todos) =>
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        )
+      ),
+    removeTodo: (id: string) =>
+      update((todos) => todos.filter((todo) => todo.id !== id)),
+    clearAllCompleted: () =>
+      update((todos) => todos.filter((todo) => !todo.completed)),
+  };
+};
+
+export const todos = createTodoStore();
