a97c74a36444a4ca837102e8ed27bf988d26c1b1
Kei-K23 <keiksl2301@gmail.com>
2024-10-22 21:17:25 +0630

    Add basic input form to create new todo

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import TodoInput from "./lib/TodoInput.svelte";
 </script>
 
 <main class="h-screen bg-neutral-800 p-4">
@@ -6,5 +7,6 @@
     <h1 class="text-orange-400 text-center text-xl md:text-3xl font-bold">
       Svelte TodoAPP
     </h1>
+    <TodoInput />
   </div>
 </main>
---
+++ src/lib/TodoInput.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { todos } from "./todo-store";
+
+  let newTodo = "";
+
+  const handleOnSubmit = () => {
+    if (newTodo.trim()) {
+      todos.addTodo(newTodo.trim());
+      newTodo = "";
+    }
+  };
+</script>
+
+<form on:submit|preventDefault={handleOnSubmit}>
+  <input type="text" bind:value={newTodo} placeholder="Add a new todo..." />
+  <button type="submit">Add todo</button>
+</form>
