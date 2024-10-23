05a6a39a99f5459c1406fa12458c9ba5bca78bea
Kei-K23 <keiksl2301@gmail.com>
2024-10-23 11:00:46 +0630

    Add style to input form to create new todo

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,9 @@
   import TodoInput from "./lib/TodoInput.svelte";
 </script>
 
-<main class="h-screen bg-neutral-800 p-4">
+<main class="h-screen bg-neutral-900 p-4 text-white">
   <div class="container mx-auto">
-    <h1 class="text-orange-400 text-center text-xl md:text-3xl font-bold">
+    <h1 class="text-rose-500 text-center text-xl md:text-3xl font-bold">
       Svelte TodoAPP
     </h1>
     <TodoInput />
--- src/lib/TodoInput.svelte
+++ src/lib/TodoInput.svelte
@@ -11,7 +11,21 @@
   };
 </script>
 
-<form on:submit|preventDefault={handleOnSubmit}>
-  <input type="text" bind:value={newTodo} placeholder="Add a new todo..." />
-  <button type="submit">Add todo</button>
-</form>
+<div class="max-w-xl flex items-center justify-center mt-8 mx-auto">
+  <form
+    on:submit|preventDefault={handleOnSubmit}
+    class="w-full flex items-center gap-2"
+  >
+    <input
+      type="text"
+      bind:value={newTodo}
+      placeholder="Add a new todo..."
+      class="outline-none px-3 py-1 rounded-md border-rose-600 border bg-neutral-900 flex-1"
+    />
+    <button
+      type="submit"
+      class="bg-neutral-700 px-2 py-1 rounded-md hover:bg-neutral-800 transition-all hover:text-white/90"
+      >Add todo</button
+    >
+  </form>
+</div>
